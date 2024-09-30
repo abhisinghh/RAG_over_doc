d4fa47da4acfc2ff66bd0aa304954d48ab0848ad
abhimanyu singh <abhimanyusinghlakhnot@gmail.com>
2024-10-01 00:06:23 +0530

    Add files via upload

---
+++ agentic_rag.py
@@ -0,0 +1,70 @@
+from langchain.document_loaders import PyPDFLoader
+from chunker import chunk_documents
+
+import yaml
+import os
+from openai import OpenAI
+
+
+with open('config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
+
+os.environ["OPENAI_API_KEY"] = config['openai_api_key']
+
+
+
+## LOADING THE PDF TO DOCUMENTS
+loader = PyPDFLoader("./content.pdf")
+documents = loader.load()
+
+
+
+vector_embedding_model = config["vector_embedding_model"]
+openai_client = OpenAI()
+
+
+def get_embedding(text, model=vector_embedding_model):
+    try:
+        response = openai_client.embeddings.create(input = [text], model=vector_embedding_model).data[0].embedding
+        return response
+    except Exception as e:
+        print(f"Error generating embedding for text: {text[:50]}... - {e}")
+        return None
+
+## chunking he documents
+df_chunks = chunk_documents(documents)
+
+print(df_chunks.shape)
+df_chunks['embedding'] = df['Chunks'].apply(get_embedding)
+
+
+## threshold for embedding similarity
+threshold = config['embedding_similarity_threshold']
+## max number of chunks to be retrieved from top 
+top_k = config['top_k_retrieved_chunks']
+
+##cosine siilarity function
+def cosine_similarity(embedding1, embedding2):
+    dot_product = np.dot(embedding1, embedding2)
+    norm_embedding1 = np.linalg.norm(embedding1)
+    norm_embedding2 = np.linalg.norm(embedding2)
+    return dot_product / (norm_embedding1 * norm_embedding2)
+
+
+def find_most_similar(query, df):
+    # Get the embedding for the query
+    query_embedding = get_embedding(query)
+    
+    if query_embedding is None:
+        print("Could not get embedding for the query.")
+        return None
+
+    # Calculate similarity between query embedding and each embedding in the DataFrame
+    df['similarity'] = df['embedding'].apply(lambda emb: cosine_similarity(query_embedding, emb))
+    filtered_df = df[df['similarity'] >= threshold]
+    sorted_df = filtered_df.sort_values(by='similarity', ascending=False)
+    top_k_df = sorted_df.head(top_k)
+
+    return top_k_df[['chunks', 'similarity']]
+    
+ 
---
+++ chunker.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import yaml
+from langchain.document_loaders import PyPDFLoader
+from langchain_experimental.text_splitter import SemanticChunker
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai.embeddings import OpenAIEmbeddings
+
+
+with open('config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
+
+openai_api_key = config['openai_api_key']
+
+#loader = PyPDFLoader("./content.pdf")
+#documents = loader.load()
+embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
+text_splitter = SemanticChunker(embedding_model, breakpoint_threshold_type="gradient")
+##text_splitter = RecursiveCharacterTextSplitter(
+#    # Set a really small chunk size, just to show.
+#    chunk_size=512,
+#    chunk_overlap=20,
+#    length_function=len,
+#    is_separator_regex=False,
+#)
+
+
+def chunk_documents(documents) :
+  doc_chunks = text_splitter.split_documents(documents)
+  doc_chunk_texts = [chunk.page_content for chunk in doc_chunks]
+  df_chunks = pd.DataFrame(doc_chunk_texts)
+  df_chunks.rename(columns = {0:'Chunks'}, inplace = True)
+  print(df_chunks.shape, df_chunks.head())
+  return df_chunks
